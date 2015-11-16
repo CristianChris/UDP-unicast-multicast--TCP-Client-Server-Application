d625514b297ac73dc4f2444f9da0986017dffe83
Cristian Cartofeanu <cristian.cartofeanu@gmail.com>
2015-11-16 16:03:24 +0200

    first commit

---
+++ client.js
@@ -0,0 +1,102 @@
+// Node.js modules
+var net = require('net');
+var JsonSocket = require('json-socket');
+var clientTCP = net.createServer();
+var _ = require('underscore');
+var fs = require('fs');
+var dgram = require('dgram');
+var clientUDP = dgram.createSocket('udp4');
+
+// Client set up
+var SRC_PORT = 6025;
+var PORT = 3000;
+var MULTICAST_ADDR = '239.255.255.250';
+
+// Declaring some variables
+var mavenHost = '';
+var mavenNeighbours = 0;
+var mavenEmployees = 0;
+
+// Bind the multicast port to our UDP client and execute functions
+clientUDP.bind(SRC_PORT, function () {
+    multicastNew();
+    setTimeout(mavenRequest,2000);
+});
+
+// Multicast listening
+clientUDP.on('listening', function () {
+    var address = clientUDP.address();
+    console.log('UDP Client listening on ' + address.address + ':' + address.port +' (multicast address).\n');
+    console.log('-----------------------Answeres from the nodes------------------------');
+});
+
+// Proccesing the messages that comes from nodes and saving into variables the nodes addreses
+clientUDP.on('message', function (messageFromNodes, rinfo) {
+    // Declaring and saving the information that comes from nodes
+    var host = String(messageFromNodes).substring(6, 15);
+    var neighbours = parseInt(String(messageFromNodes).substring(22,24));
+    var employees = parseInt(String(messageFromNodes).substring(39));
+    console.log('-------------Host: ' +host+':'+PORT+' has: _' +neighbours+ '_ neighbours and _'+ employees +'_ employees');
+    // Choosing the maven node by some criteria(>neighbours && >employees)
+    if (neighbours >= mavenNeighbours ) {
+        if (employees >= mavenEmployees) {
+            mavenHost = host;
+            mavenNeighbours = neighbours;
+            mavenEmployees = employees;
+        };
+    };
+    console.log('Curent Maven Host: ' +mavenHost+':'+PORT+' has: _' +mavenNeighbours+ '_ neighbours and '+ mavenEmployees +' employees');
+    console.log('----------------------------------------------------------------------');
+});
+
+// Function that sends multicast command to nodes listening on MULTICAST_ADDR
+function multicastNew() {
+    var messageClient = new Buffer ('host & neighbours & employers');
+    console.log('Sending multicast the command "'+messageClient+'" \n   to all nodes that are listening on multicast address "'+MULTICAST_ADDR+':'+PORT+'"');
+    clientUDP.send(messageClient, 0, messageClient.length, PORT , MULTICAST_ADDR, function () {
+    });
+};
+
+// Function that will create a TCP connection between the client and the maven
+//also it will request mavens data including it's neighbours as well
+//filtering and saving it into a .txt file
+function mavenRequest () {
+    console.log('Addres of our maven is: '+mavenHost+':'+PORT+' ('+mavenNeighbours+' neighbours and '+mavenEmployees+' employees)\n');
+    // Decorate a standard net.Socket with JsonSocket
+    var socket = new JsonSocket(new net.Socket());
+    // Establish a connection to maven
+    socket.connect(PORT, mavenHost, function(){
+        // Sending a command to our maven
+        socket.sendMessage({command: 'MavenRequestData'});
+        socket.on('message', function(MavenRequestData) {
+            console.log(MavenRequestData);
+            // console.log('Data recived: '+JSON.stringify(MavenRequestData)+'\n');
+            // // Applying our filter function
+            // var filterResult = filtering(MavenRequestData,100,'Computer Science');
+            // console.log('Filter result'+JSON.stringify(filterResult));
+            // // Writing our filterResult to a file
+            // fs.writeFile("./savedData.json", JSON.stringify(filterResult), function(err) {
+            //     console.log('----------------------------------------------------------------------')
+            //     console.log("Filtered result was saved in data.txt in the current directory!");
+            //     if(err) {
+            //         return console.log(err);
+            //     };
+            // });
+        });
+    });
+};
+
+// Function that filters collected data ( filter criteria: salary, departament, sorting )
+function filtering(collection,filt_sal,filt_depart) {
+    var filterDepartament = _.groupBy(collection, function(value){
+                return value.departament
+    });
+    var filterSalary = _.filter(filterDepartament[filt_depart], function(num){ return num.salary > filt_sal; });
+    var filterName = _.sortBy(filterSalary, 'lastName');
+    return filterName
+}
+
+
+
+
+
---
+++ node.js
@@ -0,0 +1,88 @@
+//----------- Setting up our node using arguments ---------------
+if (process.argv.length <= 5) {
+    console.log("Usage: node (filename.js) HOST PORT neighbours data_file");
+    console.log("Example: ( node node.js 127.0.0.3 3000 '[\"127.0.0.2\", \"127.0.0.4\"]' data_node_3.json ) ");
+    process.exit(-1);
+}
+
+var HOST = process.argv[2];
+var PORT = process.argv[3];
+var neighbours = JSON.parse(process.argv[4]);
+var data_file = process.argv[5];
+
+console.log('Host: ' + HOST);
+console.log('Port: ' + PORT);
+console.log('Neighbours: ' + neighbours);
+console.log('Data file to read from: ' + data_file);
+//---------------------------------------------------------------
+
+// Node.js modules
+var net = require('net');
+var JsonSocket = require('json-socket');
+var extend = require('extend');
+var dgram = require('dgram');
+var nodeUDP = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+var fs = require('fs')
+
+// Node set up multicast addres and multicast port
+var SRC_PORT = 6025;
+var MULTICAST_ADDR = '239.255.255.250';
+
+// Read from file data and store it to employeeDict (object of objects)
+var employeeDict =JSON.parse(fs.readFileSync('./data/'+data_file));
+
+// Counting the data
+var employeeDictLength = Object.keys(employeeDict).length;
+
+
+// Listening for multicast messages
+nodeUDP.on('listening', function () {
+    var address = nodeUDP.address();
+    console.log('UDP Node is listening on ' + address.address + ":" + address.port);
+});
+
+// Reciving multicast messages and execute commands and sends back answere
+nodeUDP.on('message', function (messageFromClient, rinfo) {
+    console.log('__Recived multicast command "'+messageFromClient+'" from the client address: ' + rinfo.address + ':' + rinfo.port );
+    if (messageFromClient=='host & neighbours & employers') {
+        numberNeighbours = neighbours.length;
+        messageFromNodes = 'Host: '+HOST+' have: '+numberNeighbours+' neighbours and '+employeeDictLength+' employees.';
+        nodeUDP.send(messageFromNodes, 0, messageFromNodes.length, rinfo.port , rinfo.address, function () {
+            console.log('____Sending answere back to the command "'+messageFromClient+'" ('+messageFromNodes+')');
+        });
+    };
+});
+
+// Bind our node's UDP connection to a PORT and add membership of the node for multicasting
+nodeUDP.bind(PORT, function () {
+    nodeUDP.addMembership(MULTICAST_ADDR);
+});
+
+// Function that will create a TCP connection that can be served between node and node
+//or node and client, will process the commands that it will recive
+//and will send the result back
+net.createServer(function(socket) {
+    socket = new JsonSocket(socket);
+    socket.on('message', function(message) {
+        if (message.command == 'RequestData') {
+            socket.sendMessage(employeeDict);
+        } else if(message.command == 'MavenRequestData'){
+            for (var i = 0; i < neighbours.length; i++) {
+                neighboursDataCollection(i);
+            }
+          setTimeout(function sendMessageClient(){socket.sendMessage(employeeDict)},500);
+        }
+    });
+}).listen(PORT, HOST);
+console.log('TCP Node is listening on ' + HOST +':'+ PORT);
+
+// Function that collects data from neighbours only and append it to it's own
+function neighboursDataCollection(i) {
+        var socket = new JsonSocket(new net.Socket());
+        socket.connect(PORT, neighbours[i], function(){
+            socket.sendMessage({command: 'RequestData'});
+            socket.on('message', function(RequestData) {
+                extend(employeeDict, RequestData);
+                });
+        });
+};
